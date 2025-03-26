59d24d92e78f20ea16629c87078b4533c1dfc4b9
Darinel <darinelcigarroa97@gmail.com>
2025-03-26 16:51:29 -0600

    install API dependence

--- database/migrations/2025_03_26_224044_create_tasks_table.php
+++ database/migrations/2025_03_26_224044_create_tasks_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 100);
+            $table->text('description')->nullable();
+            $table->enum('status', ['Pendiente', 'En Proceso', 'Completada'])->default('Pendiente');
             $table->timestamps();
         });
     }
